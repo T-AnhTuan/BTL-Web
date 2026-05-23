4b9f3f067860b50648c3abf00e3c99bc7613e897
Anh Tuấn Trần <tuan2k5.ptvn@gmail.com>
2026-05-23 22:22:16 +0700

    Test

--- QuanLyVatTu/QuanLyVatTu/obj/Debug/net10.0/QuanLyVatTu.AssemblyInfo.cs
+++ QuanLyVatTu/QuanLyVatTu/obj/Debug/net10.0/QuanLyVatTu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyVatTu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dd448bd6bede57bc893b35ae52d7fc2ab3ecf23")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyVatTu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyVatTu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
