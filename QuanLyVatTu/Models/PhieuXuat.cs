d57e8085a2fd22dc9e72b334ce68a0497e22a32c
meothieungu <liligauden2005@gmail.com>
2026-05-27 23:46:36 +0700

    PhieuXuat1
    Chỉnh sửa PX lần 1

--- QuanLyVatTu/Models/PhieuXuat.cs
+++ QuanLyVatTu/Models/PhieuXuat.cs
@@ -1,6 +1,51 @@
-﻿namespace QuanLyVatTu.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace QuanLyVatTu.Models
 {
     public class PhieuXuat
     {
+        [Key]
+        public int Id { get; set; }
+
+        // Thông tin chứng từ
+
+        [Required]
+        public DateTime NgayXuat { get; set; } = DateTime.Now;
+
+        [Required]
+        public string SoPhieu { get; set; } = string.Empty;
+
+        public string? DonVi { get; set; }
+
+        public string? DiaChi { get; set; }
+
+        // Định khoản kế toán
+        public string? TaiKhoanNo { get; set; }
+
+        public string? TaiKhoanCo { get; set; }
+
+        // Thông tin xuất kho
+        public string? NguoiMua { get; set; }
+
+        public string? XuatTaiKho { get; set; }
+
+        public string? DiaDiemKho { get; set; }
+
+        public string? LyDoXuat { get; set; }
+    
+        //Thông tin kỹ nhận, tổng kết
+        public int NguoiLapId { get; set; }
+
+        [ForeignKey("NguoiLapId")]
+        public NguoiDung? NguoiLap { get; set; }
+
+        public string? ThuKho { get; set; }
+
+        public string? KeToanTruong { get; set; }
+
+        public string? NguoiNhanHang { get; set; }
+
+        public ICollection<ChiTietPhieuXuat> ChiTietPhieuXuats { get; set; }
+            = new List<ChiTietPhieuXuat>();
     }
 }
