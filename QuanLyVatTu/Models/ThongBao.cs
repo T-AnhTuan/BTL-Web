79cf12d5cffe94af31f774b3a707d00b01c252e6
meothieungu <liligauden2005@gmail.com>
2026-05-28 00:07:29 +0700

    ThongBao
    Cập nhật lần 1

--- QuanLyVatTu/Models/ThongBao.cs
+++ QuanLyVatTu/Models/ThongBao.cs
@@ -1,6 +1,30 @@
-﻿namespace QuanLyVatTu.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace QuanLyVatTu.Models
 {
     public class ThongBao
     {
+        [Key]
+        public int Id { get; set; }
+
+        // Tiêu đề & nội dung
+        [Required]
+        [StringLength(100)]
+        public string TieuDe { get; set; } = string.Empty;
+
+        [Required]
+        public string NoiDung { get; set; } = string.Empty;
+
+        // Thời gian tạo
+        public DateTime NgayTao { get; set; } = DateTime.Now;
+
+        // Trạng thái
+        public bool DaXem { get; set; } = false;
+
+        // Người nhận thông báo
+        public int NguoiDungId { get; set; }
+
+        [ForeignKey("NguoiDungId")]
+        public NguoiDung NguoiDung { get; set; } = null!;
     }
 }
