bcdcd64b78906797beed30c8caefe44bfd494399
meothieungu <liligauden2005@gmail.com>
2026-05-28 00:06:20 +0700

    NhatKyHeThong
    Cập nhật lần 1

--- QuanLyVatTu/Models/NhatKyHeThong.cs
+++ QuanLyVatTu/Models/NhatKyHeThong.cs
@@ -1,6 +1,33 @@
-﻿namespace QuanLyVatTu.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace QuanLyVatTu.Models
 {
     public class NhatKyHeThong
     {
+        [Key]
+        public int Id { get; set; }
+
+        // Người thực hiện
+        public int NguoiDungId { get; set; }
+
+        [ForeignKey("NguoiDungId")]
+        public NguoiDung NguoiDung { get; set; } = null!;
+
+        // Nội dung thao tác
+        [Required]
+        [StringLength(150)]
+        public string HanhDong { get; set; } = string.Empty;
+
+        // Ví dụ:
+        // "Đăng nhập hệ thống"
+        // "Tạo phiếu nhập"
+        // "Xóa vật tư"
+
+        // Thời gian
+        public DateTime ThoiGian { get; set; } = DateTime.Now;
+
+        // Địa chỉ IP (nếu cần)
+        [StringLength(50)]
+        public string? DiaChiIP { get; set; }
     }
 }
