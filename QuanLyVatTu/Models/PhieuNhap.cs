8f4b332a11dbd3319e4ed393aee92b48116642d2
meothieungu <liligauden2005@gmail.com>
2026-05-27 23:45:06 +0700

    PhieuNhap2
    Chỉnh sửa PN lần 2

--- QuanLyVatTu/Models/PhieuNhap.cs
+++ QuanLyVatTu/Models/PhieuNhap.cs
@@ -12,28 +12,28 @@ namespace QuanLyVatTu.Models
         [Required]
         public DateTime NgayNhap { get; set; } = DateTime.Now;//ngày..
         public string SoPhieu { get; set; } = string.Empty;//số:...
-        public string DonVi { get; set; }//đơn vị
-        public string DiaChi { get; set; }//bộ phận
+        public string? DonVi { get; set; }//đơn vị
+        public string? DiaChi { get; set; }//bộ phận
 
         //Đinh khoản kế toán
-        public string TaiKhoanNo { get; set; }       // Nợ: ......
-        public string TaiKhoanCo { get; set; }       // Có: ......
+        public string? TaiKhoanNo { get; set; }       // Nợ: ......
+        public string? TaiKhoanCo { get; set; }       // Có: ......
 
         //Thông tin giao nhận
-        public string NhaCungCap { get; set; }   // Tên nhà cung cấp
-        public string NhapTaiKho { get; set; }    // Tên kho: Nhập hàng
-        public string DiaDiemKho { get; set; }       // Địa điểm kho
+        public string? NhaCungCap { get; set; }   // Tên nhà cung cấp
+        public string? NhapTaiKho { get; set; }    // Tên kho: Nhập hàng
+        public string? DiaDiemKho { get; set; }       // Địa điểm kho
 
         //Thông tin kỹ nhận, tổng kết
         public int NguoiLapId { get; set; }
         [ForeignKey("NguoiLapId")]
-        public NguoiDung NguoiLap { get; set; }    
-        public string NguoiGiaoHang { get; set; }    
-        public string ThuKho { get; set; }           
-        public string KeToanTruong { get; set; }
+        public NguoiDung NguoiLap { get; set; } = null!; 
+        public string? NguoiGiaoHang { get; set; }    
+        public string? ThuKho { get; set; }           
+        public string? KeToanTruong { get; set; }
 
         // Mối quan hệ 1 - Nhiều với Chi tiết phiếu nhập
         public ICollection<ChiTietPhieuNhap> ChiTietPhieuNhaps { get; set; }
-           = new List<ChiTietPhieuNhap>();
+            = new List<ChiTietPhieuNhap>();
     }
 }
