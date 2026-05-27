334a520b1fa308bd78ef6d5c90de83d3fa047454
meothieungu <liligauden2005@gmail.com>
2026-05-27 23:53:42 +0700

    NguoiDung1
    Chỉnh sửa NguoiDung lần 1

--- QuanLyVatTu/Models/NguoiDung.cs
+++ QuanLyVatTu/Models/NguoiDung.cs
@@ -1,6 +1,26 @@
-﻿namespace QuanLyVatTu.Models
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace QuanLyVatTu.Models
 {
     public class NguoiDung
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string? TenDangNhap { get; set; } 
+
+        [Required]
+        public string? MatKhau { get; set; } // Bảo mật: Không lưu mật khẩu thô
+
+        [Required, StringLength(100)]
+        public string? HoTen { get; set; } 
+
+        public bool IsKhóa { get; set; } = false;
+
+        public int PhanQuyenId { get; set; }
+
+        [ForeignKey("PhanQuyenId")]
+        public PhanQuyen PhanQuyen { get; set; } = null!;
     }
 }
