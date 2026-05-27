294844003bef2933ece6cfcb0c6e8fe9ebfb46e7
Anh Tuấn Trần <tuan2k5.ptvn@gmail.com>
2026-05-27 22:56:15 +0700

    Revert "PhanQuyen1"
    This reverts commit cf7b46a6c3e1fe2fd3cc1753da256e077fb58eb6.

--- QuanLyVatTu/Models/PhanQuyen.cs
+++ QuanLyVatTu/Models/PhanQuyen.cs
@@ -1,19 +1,10 @@
-using System.ComponentModel.DataAnnotations;  // key, requird,StringLength 
-using System.ComponentModel.DataAnnotations.Schema; //
 namespace QuanLyVatTu.Models
 {
     public class PhanQuyen
     {
-        [Key]
         public int Id { get; set; }
-
-        [Required, StringLength(30)]
-        public string TenNhomQuyen { get; set; } = string.Empty;
-
-        public string MoTa { get; set; } = string.Empty;
-
-       // Mối quan hệ: Một quyền có nhiều Người dùng
-       public ICollection<NguoiDung> NguoiDung { get; set; }
-         = new List<NguoiDung>();
+        public string RoleName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public ICollection<NguoiDung>? Users { get; set; }
     }
 }
